5bd737229e8d3fb0abd0b88798ae2eeb5e46c400
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-05-08 15:38:50 -0400

    added to the post model

--- models/Post.js
+++ models/Post.js
@@ -6,15 +6,30 @@ const sequelize = require("../config/config");
 class Post extends Model {}
 
 Post.init({
+    title: {
+        type: DataType.STRING
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        refrences: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    content: {
 
+        type: DataTypes.TEXT
+
+    },
     
-
-
-
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
 
         
 
 });
 
 module.exports = Post;
-    // finish this Comment Model
+    // finish this POST model 
