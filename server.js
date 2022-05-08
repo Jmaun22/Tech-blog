52dfd32b9533bc8dcec6d26768b2f9170d43ff53
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-05-08 15:02:04 -0400

    add requried for the server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+// You need the following required:
+// express
+const express = require('express');
+// path
+const path = require('path');
+
+// express-handlebars
+const express_Handlebars = require('express-handlebars');
+
+// helpers (if you are putting timestamps on posts)
+const helper = require('./utils/helper')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/config");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+    // For password sessions
+    
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
+});
