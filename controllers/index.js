23ebc7ed8c143b1d7b447726b6b716278043dae0
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-05-08 15:15:42 -0400

    added to routes to index in contollers

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 
 // finish the required routes
-// const apiRoutes =
-// const homeRoutes =
-// const dashboardRoutes =
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/dash", dashboardRoutes);
 // finish the other needed routes with router.use()
 
 module.exports = router;
